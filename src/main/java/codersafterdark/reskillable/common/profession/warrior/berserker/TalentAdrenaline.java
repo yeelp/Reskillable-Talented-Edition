e1987bc343a83e725cc39364438d513c09293560
Heaveness <44594469+Heaveness@users.noreply.github.com>
2021-04-21 19:47:28 -0400

    Update TalentAdrenaline.java

--- src/main/java/codersafterdark/reskillable/common/profession/warrior/berserker/TalentAdrenaline.java
+++ src/main/java/codersafterdark/reskillable/common/profession/warrior/berserker/TalentAdrenaline.java
@@ -10,6 +10,5 @@ public class TalentAdrenaline extends Talent {
         super(new ResourceLocation(MOD_ID, "adrenaline"), 2, 1, new ResourceLocation(MOD_ID, "warrior"), new ResourceLocation(MOD_ID, "berserker"),
                 3, "reskillable:attack 10", "reskillable:defense 8");
         setCap(5);
-        this.setIcon(new ResourceLocation("textures/items/iron_sword.png"));
     }
 }
