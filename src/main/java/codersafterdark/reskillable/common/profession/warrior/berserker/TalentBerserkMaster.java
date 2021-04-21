497598cd55f71717171f0176a2fdf8255f081b2a
Heaveness <44594469+Heaveness@users.noreply.github.com>
2021-04-21 19:48:16 -0400

    Update TalentBerserkMaster.java

--- src/main/java/codersafterdark/reskillable/common/profession/warrior/berserker/TalentBerserkMaster.java
+++ src/main/java/codersafterdark/reskillable/common/profession/warrior/berserker/TalentBerserkMaster.java
@@ -10,6 +10,5 @@ public class TalentBerserkMaster extends Talent {
         super(new ResourceLocation(MOD_ID, "berserk_master"), 1, 0, new ResourceLocation(MOD_ID, "warrior"), new ResourceLocation(MOD_ID, "berserker"),
                 3, "reskillable:attack 10", "reskillable:defense 8");
         setCap(1);
-        this.setIcon(new ResourceLocation("textures/items/iron_sword.png"));
     }
 }
