f1f1fe0688b196d96d963745117930a31353007a
Heaveness <44594469+Heaveness@users.noreply.github.com>
2021-04-21 19:48:48 -0400

    Update TalentEdge.java

--- src/main/java/codersafterdark/reskillable/common/profession/warrior/berserker/TalentEdge.java
+++ src/main/java/codersafterdark/reskillable/common/profession/warrior/berserker/TalentEdge.java
@@ -10,6 +10,5 @@ public class TalentEdge extends Talent {
         super(new ResourceLocation(MOD_ID, "edge"), 2, 4, new ResourceLocation(MOD_ID, "warrior"), new ResourceLocation(MOD_ID, "berserker"),
                 3, "reskillable:attack 10", "reskillable:defense 8");
         setCap(5);
-        this.setIcon(new ResourceLocation("textures/items/iron_sword.png"));
     }
 }
